b7d354a8d3ff3f0a159e833a5ed12af81c321ed1
OnePartHarmony <bakingharmony@gmail.com>
2022-09-15 00:08:48 -0400

    basic sudo outline of project

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// empty array for objects which will each be {shape:,color:,background:,nuumber}
+//array of shapes
+// array of colors
+// array of backgrounds
+// array of numbers
+
+//fill array with 81 unique shapes
+
+// button fills 12 divs
+
+//click shape highlights
+//click 3 shapes removes or alerts if not set
+//if removed, auto-refill with delay
+
+//other button checks for possible set and adds 3 or alerts
+
+
+
