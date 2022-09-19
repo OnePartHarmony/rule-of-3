224ebd29e518e1f1a6bfeb04074f8931b68dfdff
OnePartHarmony <bakingharmony@gmail.com>
2022-09-19 15:08:56 -0400

    paused working on visuals to work more on functions that create unique cards

--- js/index.js
+++ js/index.js
@@ -4,27 +4,55 @@ const deck = document.getElementById("deck")
 
 
 
-// empty array for array which will each be [shape, color, background, number]
+// empty array for arrays which will each hold a unique combination of property variations
 let possibleCards = []
-const fill = ["solid", "hollow", "stripe"]
-const numbers = [1,2,3]
+//constructor for an object to hold properties, so easy-hard modes can create cards with 2-4 properties
+class PropertyObject {
+    constructor(shapes, colors, fill, numbers) {
+        this.shapes = shapes
+        this.colors = colors        
+        this.fill = fill
+        this.numbers = numbers
+    }
+}
+
 const shapes = ["triangle", "circle", "square"]
 const colors = ["red", "green", "blue"]
+const fill = ["solid", "hollow", "stripe"]
+const numbers = [1,2,3]
+
+// const testObject = new PropertyObject(shapes, colors, fill, numbers)
+// console.log(Object.keys(testObject).length)
+
 
 //Here should be a function that exactly sorts the properties into propertyArrays, with no two alike
 
+const arrayOfUniqueArrays = (propertyObject) => {
+    propertyObject.colors.forEach(color => {
+        for (i=0; i > (Object.keys(cardObject).length / 3); i++) {
+            possibleCards.push([color])
+        }
+    })
+}
+
+
+// const createObject = () => {
+//     return new PropertyObject(shapes, colors, fill)
+// }
+// const test = createObject()
+// console.log(test)
 
 
 
 
 // this function creates cards
 
-possibleCards.forEach((propertyArray, index) => {
+possibleCards.forEach((uniquePropertyArray, index) => {
         card = document.createElement("div")
         card.setAttribute("id", `${index}`)
         card.classList.add("card")
         //This assigns classes based on properties
-        for (word of propertyArray) {
+        for (word of uniquePropertyArray) {
             card.classList.add(word)
         }
         deck.appendChild(card)
