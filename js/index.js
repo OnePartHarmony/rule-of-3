0731a91993bff0c73506f8db205e73649e78b086
OnePartHarmony <bakingharmony@gmail.com>
2022-09-19 19:56:17 -0400

    function makeCards now sets the number of shapes

--- js/index.js
+++ js/index.js
@@ -5,23 +5,29 @@ const deck = document.getElementById("deck")
 //arrays of possibilities for each property
 const shapes = ["triangle", "circle", "square"]
 const colors = ["red", "green", "blue"]
-const fill = ["solid", "hollow", "stripe"]
 const numbers = [1,2,3]
+const fill = ["solid", "hollow", "stripe"]
+
 
 //this function creates an array of arrays, each with a unique combination of one each of the properties
 const uniqueSort = (...a) => a.reduce((a, b) => a.flatMap(d => b.map(e => [d, e].flat())))
-possibleCards = uniqueSort(shapes, colors, fill, numbers)
+possibleCardsEasy = uniqueSort(shapes, colors, numbers)
+possibleCardsRegular = uniqueSort(shapes, colors, numbers, fill)
 // console.log(possibleCards)
 
 
 
-// this function creates cards
-
-possibleCards.forEach((uniqueCardArray, index) => {
-    //create a div with a unique id
-        card = document.createElement("div")
-        cardShape = document.createElement("div")
-        card.appendChild(cardShape)
+// this function creates cards based on the regular array of 81 or the easy array of 27
+const makeCards = (arrayOfCards) => {
+    arrayOfCards.forEach((uniqueCardArray, index) => {
+        //create a div with a unique id
+        const card = document.createElement("div")
+        //create shapes inside the div, quantity depending on number property
+        let intNumber = parseInt(uniqueCardArray[2])
+        for (let i=1; i <= intNumber; i++ ) {
+            let cardShape = document.createElement("div")
+            card.appendChild(cardShape)
+        }
         card.setAttribute("id", `${index}`)
         card.classList.add("card")
         //This assigns classes based on properties
@@ -31,10 +37,10 @@ possibleCards.forEach((uniqueCardArray, index) => {
         firstTwelve.appendChild(card)
         //This assigns the div to a variable that uses the index in the name
         window[`card${index}`] = card
-})
+    })
+}
 
-
-
+makeCards(possibleCardsRegular)
 
 
 
