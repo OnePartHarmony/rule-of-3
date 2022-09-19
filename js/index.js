53fde44102458e0228bd1dd512612a4773f9cfb6
OnePartHarmony <bakingharmony@gmail.com>
2022-09-19 17:03:48 -0400

    deleted the extra lines of code where I was doing things the hard way

--- js/index.js
+++ js/index.js
@@ -1,21 +1,6 @@
-// const mat = document.getElementById("mat")
-// const startGame = document.getElementById("start-game")
-// const deck = document.getElementById("deck")
-
-
-
-// // empty array for arrays which will each hold a unique combination of property variations
-// let possibleCards = []
-
-// //constructor for an object to hold properties, so easy or hard modes can create cards with 3 or 4 properties
-// class PropertyObject {
-//     constructor(shapes, colors, fill, numbers) {
-//         this.shapes = shapes
-//         this.colors = colors        
-//         this.fill = fill
-//         this.numbers = numbers
-//     }
-// }
+const mat = document.getElementById("mat")
+const startGame = document.getElementById("start-game")
+const deck = document.getElementById("deck")
 
 //arrays of possibilities for each property
 const shapes = ["triangle", "circle", "square"]
@@ -27,65 +12,6 @@ const numbers = [1,2,3]
 const uniqueSort = (...a) => a.reduce((a, b) => a.flatMap(d => b.map(e => [d, e].flat())))
 let possibleCards = uniqueSort(shapes, colors, fill, numbers)
 console.log(possibleCards)
-
-
-//Here should be a function that exactly sorts the property possibilities into propertyArrays, with no two alike
-
-// const arrayOfUniqueArrays = (propertyObject) => {
-//     propertyObject.shapes.forEach(shape => {
-//         let exponent = (Object.keys(propertyObject).length - 1)
-//         for (i=0; i < (3 ** exponent); i++) {
-//             possibleCards.push([shape])
-//         }
-//     })
-//     let index = 0
-//     for(i=0; i < possibleCards.length; i + (possibleCards.length / 3) ) {
-//         propertyObject.colors.forEach(color => {
-//             for (i=0; i < (possibleCards.length / 9); i++) {
-//                 possibleCards[index].push(color)
-
-//                 console.log( `after index ${i} has been updated, the array of unique arrays is`, possibleCards)
-//                 index++
-//             }
-//         })
-    // }
-    // index = 0
-    // for(i=0; i < possibleCards.length; i + (possibleCards.length / 9) ) {
-    //     propertyObject.colors.forEach(fill => {
-    //         for (i=0; i < (possibleCards.length / 27); i++) {
-    //             possibleCards[index].push(fill)
-    //             index++
-    //         }
-    //     })
-    // }
-    // index = 0
-    // if (propertyObject.numbers) {
-    // //since the most properties we can have is 4, we know this array is 81 long
-    //     for(i=0; i < 81; i + 3) {
-    //         propertyObject.colors.forEach(fill => {
-    //                 possibleCards[index].push(fill)
-    //                 index++
-    //         })
-    //     }
-    // }
-// }
-
-
-// const testObject = new PropertyObject(shapes, colors, fill, numbers)
-// arrayOfUniqueArrays(testObject)
-// console.log(possibleCards)
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
