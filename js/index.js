76d57089fd3bf3755483c8d5face01ea95b4f326
OnePartHarmony <bakingharmony@gmail.com>
2022-09-19 16:04:59 -0400

    this code should be working, but I've got an error to fix

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const deck = document.getElementById("deck")
 
 // empty array for arrays which will each hold a unique combination of property variations
 let possibleCards = []
-//constructor for an object to hold properties, so easy-hard modes can create cards with 2-4 properties
+//constructor for an object to hold properties, so easy-hard modes can create cards with 3-4 properties
 class PropertyObject {
     constructor(shapes, colors, fill, numbers) {
         this.shapes = shapes
@@ -21,44 +21,59 @@ const colors = ["red", "green", "blue"]
 const fill = ["solid", "hollow", "stripe"]
 const numbers = [1,2,3]
 
-// const testObject = new PropertyObject(shapes, colors, fill, numbers)
-// console.log(Object.keys(testObject).length)
+const testObject = new PropertyObject(shapes, colors, fill, numbers)
+
 
 
 //Here should be a function that exactly sorts the properties into propertyArrays, with no two alike
 
 const arrayOfUniqueArrays = (propertyObject) => {
-    propertyObject.colors.forEach(color => {
-        for (i=0; i > (Object.keys(cardObject).length / 3); i++) {
-            possibleCards.push([color])
+    propertyObject.shapes.forEach(shape => {
+        for (i=0; i < ((Object.keys(propertyObject).length - 1) ** 3); i++) {
+            possibleCards.push([shape])
         }
     })
+    let index = 0
+    for(i=0; i < possibleCards.length; i + (possibleCards.length / 3) ) {
+        propertyObject.colors.forEach(color => {
+            for (i=0; i < (possibleCards.length / 9); i++) {
+                possibleCards[index].push(color)
+                index++
+            }
+        })
+    }
+    index = 0
+    for(i=0; i <= possibleCards.length; i + (possibleCards.length / 9) ) {
+        propertyObject.colors.forEach(fill => {
+            for (i=0; i < (possibleCards.length / 27); i++) {
+                possibleCards[index].push(fill)
+                index++
+            }
+        })
+    }
+
 }
+arrayOfUniqueArrays(testObject)
 
 
-// const createObject = () => {
-//     return new PropertyObject(shapes, colors, fill)
-// }
-// const test = createObject()
-// console.log(test)
 
 
 
 
 // this function creates cards
 
-possibleCards.forEach((uniquePropertyArray, index) => {
-        card = document.createElement("div")
-        card.setAttribute("id", `${index}`)
-        card.classList.add("card")
-        //This assigns classes based on properties
-        for (word of uniquePropertyArray) {
-            card.classList.add(word)
-        }
-        deck.appendChild(card)
-        //This assigns the div to a variable that uses the index in the name
-        window[`card${index}`] = card
-})
+// possibleCards.forEach((uniquePropertyArray, index) => {
+//         card = document.createElement("div")
+//         card.setAttribute("id", `${index}`)
+//         card.classList.add("card")
+//         //This assigns classes based on properties
+//         for (word of uniquePropertyArray) {
+//             card.classList.add(word)
+//         }
+//         deck.appendChild(card)
+//         //This assigns the div to a variable that uses the index in the name
+//         window[`card${index}`] = card
+// })
 
 
 
