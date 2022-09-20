73f1271e56fed1b13aea51d1cb2aeada34cfd265
OnePartHarmony <bakingharmony@gmail.com>
2022-09-20 16:10:44 -0400

    layout and styling are nearly there, buttons work.

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const rules = document.getElementById("rules")
 const showRules1 = document.getElementById("rules-show1")
 const showRules2 = document.getElementById("rules-show2")
 const hideRules = document.getElementById("rules-close")
-const cardDiv = document.getElementById("card-div")
+const cardMat = document.getElementById("card-mat")
 const deck = document.getElementById("deck")
 const message = document.getElementById("message")
 const newGame = document.getElementById("new-game")
@@ -55,9 +55,9 @@ const shuffleCards = (arrayOfCards) => {
 // this function creates cards based on the regular array of 81 or the easy array of 27
 const makeCards = (arrayOfCards) => {
     arrayOfCards.forEach((uniqueCardArray, index) => {
-        //create a div with a unique id
-        const card = document.createElement("div")
-        //create shapes inside the div, quantity depending on number property
+        //create a li with a unique id
+        const card = document.createElement("li")
+        //create shapes inside the li, quantity depending on number property
         let intNumber = parseInt(uniqueCardArray[2])
         for (let i=1; i <= intNumber; i++ ) {
             let cardShape = document.createElement("div")
@@ -65,12 +65,13 @@ const makeCards = (arrayOfCards) => {
         }
         card.setAttribute("id", `${index}`)
         card.classList.add("card")
+        card.classList.add("back")
         //This assigns classes based on properties
         for (const word of uniqueCardArray) {
             card.classList.add(word)
         }
-        cardDiv.appendChild(card)
-        //This assigns the div to a variable that uses the index in the name
+        deck.appendChild(card)
+        //This assigns the li to a variable that uses the index in the name
         window[`card${index}`] = card
     })
 }
@@ -79,7 +80,7 @@ const makeCards = (arrayOfCards) => {
 const startGame = (arrayOfCards) => {
     shuffleCards(arrayOfCards)
     makeCards(arrayOfCards)
-     message.innerText = "Click on the deck to deal"
+     message.innerText = "Click on the deck to deal."
     changeDisplay(titleScreen)
 }
 
@@ -88,16 +89,27 @@ startRegularGame.addEventListener("click", function() {startGame(possibleCardsRe
 
 //the new game button removes all the cards and returns to the title screen
 newGame.addEventListener("click", function() {
-    while (cardDiv.firstChild) {
-        cardDiv.removeChild(cardDiv.firstChild)
+    while (cardMat.firstChild) {
+        cardMat.removeChild(cardMat.firstChild)
+    }
+    while (deck.firstChild) {
+        deck.removeChild(deck.firstChild)
     }
     changeDisplay(titleScreen)
 })
 
 
 
-//click shape highlights
-//click 3 shapes removes or alerts if not set
+
+
+
+
+
+
+
+
+//click card highlights
+//click 3 cards removes or alerts if not set
 //if removed, auto-refill with delay
 
 //other button checks for possible set and adds 3 or alerts
